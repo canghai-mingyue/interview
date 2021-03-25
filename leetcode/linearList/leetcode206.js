8a724a929683b4caed1c21684eef0e85d02347b1
roborock-hexin <hexin@roborock.com>
2021-03-25 15:14:46 +0800

    construct init

---
+++ leetcode/linearList/leetcode206.js
@@ -0,0 +1,24 @@
+// leetcode 206 反转链表
+
+/*
+    反转一个单链表。
+    示例：
+    输入: 1->2->3->4->5->NULL
+    输出: 5->4->3->2->1->NULL
+ */
+
+/**
+ * Definition for singly-linked list.
+ */
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function(head) {
+
+};
